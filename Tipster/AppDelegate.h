c27e22c90dc25f4fd283d96e8ab7c325b55ca692
Daniela Guajardo <danyguajiba@fb.com>
2019-06-25 13:52:46 -0700

    Initial Commit

---
+++ Tipster/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Tipster
+//
+//  Created by danyguajiba on 6/25/19.
+//  Copyright © 2019 danyguajiba. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Tipster/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Tipster
+//
+//  Created by danyguajiba on 6/25/19.
+//  Copyright © 2019 danyguajiba. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
